9ab1f147f02a931ac9dc871bf7bbdeded76ee617
phplime <mdnazmul.nm@gmail.com>
2025-11-01 10:52:32 +0600

    Create api.php

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Request;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Admin\DashboardController;
+
+// Public routes
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    // Route::get('/profile', [ProfileController::class, 'show']);
+});
